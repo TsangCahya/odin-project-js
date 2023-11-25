186fab038355cc500826b5c2adca7d32536e0704
Tsang Cahya <67121407+TsangCahya@users.noreply.github.com>
2023-11-26 02:01:26 +0700

    addint setInterval

--- digitalClock/index.js
+++ digitalClock/index.js
@@ -2,10 +2,12 @@ const hours= document.querySelector(".hours__number");
 const mins=document.querySelector(".MINS__number");
 const sec=document.querySelector(".SEC__number");
 
-const currentTime= new Date();
+setInterval(()=>{
+    const currentTime= new Date();
 let jam =currentTime.getHours();
 let menit=currentTime.getMinutes();
 let detik=currentTime.getSeconds();
 hours.textContent= (jam<10? "0" + jam:jam);
 mins.textContent=(menit<10? "0"+menit:menit);
 sec.textContent=(detik<10?"0"+detik:detik);
+},1000);
